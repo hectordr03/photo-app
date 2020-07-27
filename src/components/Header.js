2bc55a69a3eb12543a9c8618fd78522be92e61f7
Student_GA126 <student_ga126@psxga126.lan>
2020-07-27 15:25:49 -0400

    Finally centered Header elements

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,18 +5,28 @@ import React from 'react';
 import '../App.css';
 
 import profilePic from '../assests/images/profile-pic.png'
+
+// Bootstrap imports
 import Image from 'react-bootstrap/Image'
+import Dropdown from 'react-bootstrap/Dropdown'
 
 class Header extends React.Component {
     render() {
         return(
             <div className='header'>
                 {/* Bootstrap image comp for circle image style */}
-                <Image src={profilePic} roundedCircle id='header-profile-pic' />
+                <div className='message'>
+                    <Image src={profilePic} roundedCircle id='header-profile-pic' />
+                    <h4>Welcome user!</h4>
+                </div>
 
-                <h4>Welcome user!</h4>
-                
                 <ul id='nav'>
+                    <Dropdown.Menu show>
+                        <Dropdown.Header>Friends</Dropdown.Header>
+                        <Dropdown.Item eventKey="2">Friend 1</Dropdown.Item>
+                        <Dropdown.Item eventKey="3">Friend 2</Dropdown.Item>
+                        <Dropdown.Item eventKey="4">Friend 3</Dropdown.Item>
+                    </Dropdown.Menu>
                     <a className='nav-item' href='#'>
                         <li>Friends</li>
                     </a>
