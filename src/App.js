e2024158371678025b9ce89140b81c0f11de64ce
Student_GA126 <student_ga126@psxga126.lan>
2020-07-27 13:36:46 -0400

    Import react-router-dom elements

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import './App.css';
 
+// Components
 import Header from './components/Header'
 import Feed from './components/Feed'
+
+// React-Router
+// "BrowserRouter as Router" allows me to rename BrowserRouter to Router for simplicity
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Feed />
+      
     </div>
   );
 }
